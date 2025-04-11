c96d52d71b5042ddf5951e6aca87a4fa3cb72914
Charle <watrincharle.pro@gmail.com>
2025-04-11 16:53:45 +0200

    organisation

--- shoot.lua
+++ shoot.lua
@@ -1,16 +1,17 @@
 shoot = {}
 
 function shoot.load(pShooter, pIndex)
-    local s = {
-        index = pIndex,
-        x = pShooter.x,
-        y = pShooter.y,
-        rotation = pShooter.rotation,
-        speed = 100,
-        sprite = love.graphics.newImage("sprites/bullet.png"),
-        width = s.sprite:getWidth(),
-        height = s.sprite:getHeight()
-    }
+    local sprite = love.graphics.newImage("sprites/bullet.png")
+    local s = {}
+        s.index = pIndex
+        s.x = pShooter.x or 100
+        s.y = pShooter.y or 100
+        s.rotation = pShooter.rotation or 0
+        s.speed = 100
+        s.sprite = sprite
+        s.width = s.sprite:getWidth()
+        s.height = s.sprite:getHeight()
+    
 
     function s:update(dt)
         s.x = s.x + s.speed * dt * math.cos(s.rotation)
@@ -21,6 +22,7 @@ function shoot.load(pShooter, pIndex)
         love.graphics.draw(s.sprite, s.x, s.y, s.rotation)
     end
 
+    return s 
 end
 
 return shoot
