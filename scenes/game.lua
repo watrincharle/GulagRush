1fa6cbcc2295a7f0f9e88064e2230ebbec4b5f8e
Charle <watrincharle.pro@gmail.com>
2025-04-12 16:39:42 +0200

    refacto - adding game scene

---
+++ scenes/game.lua
@@ -0,0 +1,30 @@
+game = {}
+
+function game:update(dt)
+    mX, mY = love.mouse.getPosition()
+        map.update()
+        hero:update(dt)
+        if #ennemies > 0 then
+            for _, e in ipairs(ennemies) do
+                e:update(dt)  
+            end
+            for i = #ennemies, 1, -1 do
+                if ennemies[i].isFree == true then
+                    table.remove(ennemies, i)
+                end
+            end
+        end
+        if #bullets > 0 then
+            for _, s in ipairs(bullets) do
+                s:update(dt) 
+            end
+            for i = #bullets, 1, -1 do
+                if bullets[i].isFree == true then
+                    table.remove(bullets, i)
+                end
+            end
+        end
+        ammoManager.update(dt)
+end
+
+return game
